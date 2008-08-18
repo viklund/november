55772d5562bd86e3ac401da11e3ace63fb8df0b9
Carl Masak <masak@juno.local>
2008-08-18 18:49:45 +0200

    [p6w] added more helpful comment when PARROT_DIR is not set

--- p6w/test_wiki.sh
+++ p6w/test_wiki.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 QS=$1
-set -u PARROT_DIR
+if [ "$PARROT_DIR" == "" ]; then
+  echo Hello, we\'d like you to set your PARROT_DIR first.
+  echo If you\'re using bash, you do it like this:
+  echo export PARROT_DIR=/path/to/your/installation/of/parrot/
+  echo Also consider putting this line into your \~/.bashrc for great justice.
+  exit
+fi
 REQUEST_METHOD=GET \
 QUERY_STRING=$QS \
 HTTP_COOKIE='session_id=673766.5765' \
