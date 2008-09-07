79430f7482ec31c4a20861e8986153c4a48eec54
Ilya Belikin <forihrd@gmail.com>
2008-09-07 13:56:58 +1100

    [p6w] cgi_post.t.sh combing, m.b. CGI must to chomp $*IN, but I don`t sure about that (and Rakudo can`t do chomp right now)

--- p6w/t/02-cgi_post.t.sh
+++ p6w/t/02-cgi_post.t.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-echo "foo=bar" |  
+echo -n "foo=bar" |  
 REQUEST_METHOD='POST' \
 TEST_RESULT='{"foo" => "bar"}' \
 TEST_NAME='Post foo=bar' \
 ./t/cgi_post_test; 
 
-echo "foo=bar&boo=her" |  
+echo -n "foo=bar&boo=her" |  
 REQUEST_METHOD='POST' \
-TEST_RESULT='{"foo" => "bar", "boo" => "her\n"}' \
+TEST_RESULT='{"foo" => "bar", "boo" => "her"}' \
 TEST_NAME='Post foo=bar&boo=her' \
 ./t/cgi_post_test; 
 
-echo "foo=bar&foo=boo&boo=her" |  
+echo -n "foo=bar&foo=boo&gop=her" |  
 REQUEST_METHOD='POST' \
-TEST_RESULT='{"foo" => ["bar", "boo"], "boo" => "her"}' \
-TEST_NAME='Post foo=bar&boo=her&foo=boo' \
+TEST_RESULT='{"foo" => ["bar", "boo"], "gop" => "her"}' \
+TEST_NAME='Post foo=bar&foo=boo&gop=her' \
 ./t/cgi_post_test; 
 
 
