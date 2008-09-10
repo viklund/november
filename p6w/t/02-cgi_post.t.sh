bfd781bc9bb886de43a7cc37d6c4a30731375572
Ilya Belikin <forihrd@gmail.com>
2008-09-10 17:21:48 +1100

    [p6w/CGI.pm] add three test to 02-cgi_post.t.sh and comment about %*ENV

--- p6w/t/02-cgi_post.t.sh
+++ p6w/t/02-cgi_post.t.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Use shell script here, becouse
+# RAKUDO: Can`t modify %*ENV;
+
 echo -n "foo=bar" |  
 REQUEST_METHOD='POST' \
 TEST_RESULT='{"foo" => "bar"}' \
@@ -15,4 +18,24 @@ echo -n "test=foo&test=bar" |
 REQUEST_METHOD='POST' \
 TEST_RESULT='{"test" => ["foo", "bar"] }' \
 TEST_NAME='Post test=foo&test=bar' \
+./t/cgi_post_test; 
+
+echo -n "test=foo&test=bar&foo=bar" |  
+REQUEST_METHOD='POST' \
+TEST_RESULT='{"test" => ["foo", "bar"], "foo" => "bar"}' \
+TEST_NAME='Post test=foo&test=bar&foo=bar' \
+./t/cgi_post_test; 
+
+echo -n "test=foo" |  
+REQUEST_METHOD='POST' \
+QUERY_STRING="boom=bar" \
+TEST_RESULT='{"test" => "foo", "boom" => "bar" }' \
+TEST_NAME='Post test=foo Get boom=bar (test get and post mix)' \
+./t/cgi_post_test; 
+
+echo -n "test=foo" |  
+REQUEST_METHOD='POST' \
+QUERY_STRING="test=bar" \
+TEST_RESULT='{"test" => ["bar", "foo"] }' \
+TEST_NAME='Post test=foo Get test=bar (test get and post mix)' \
 ./t/cgi_post_test; 
