08fc4666b3f7c26aa556a9528523de1b9f268013
Ilya Belikin <forihrd@gmail.com>
2008-09-18 02:06:54 +1100

    [p6w] simple and naive (and mv ugly) tags sistem. Try it plz

--- p6w/Wiki.pm
+++ p6w/Wiki.pm
@@ -19,10 +19,33 @@ sub get_unique_id {
     return int(time%1000000/100) ~ time%100
 }
 
-# RAKUDO: :g not implemented yet :( 
 sub r_remove( $str is rw ) {
+    # RAKUDO: :g not implemented yet :( 
     while $str ~~ /\\r/ {
         $str = $str.subst( /\\r/, '' );
+    }
+}
+
+sub tags_parse ($tags) {
+    # RAKODO: we need regexp in split [perl 57378]
+    # my @tags = $tags.split(/ \s* , \s* /)
+    # workaround:
+    my @tags = $tags.split(',');
+    @tags = map { .subst(/ ^ \s+ /, '') }, @tags;
+    @tags = map { .subst(/  \s+ $ /, '') }, @tags;
+
+    @tags = map { .subst(/ \n /, '') }, @tags;
+    return @tags;
+}
+
+
+sub tag_count_normalize ($count) {
+    # TODO: log need here, now just feake
+    if $count < 10 {
+        return $count; 
+    }
+    else {
+        return 10; 
     }
 }
 
@@ -79,7 +102,22 @@ class Storage {
     method read_modification($modification_id)                   { ... }
     method write_modification( $modification_id, $modification ) { ... }
 
-    method save_page($page, $new_text, $author) {
+    method read_page_tags ($page)                                { ... }
+    method write_page_tags ($page, $tags)                        { ... }
+
+    method add_tags ($page, $tags)                               { ... }
+    method remove_tags ($page, $tags)                            { ... }
+
+    method read_tags_count                                       { ... }
+    method write_tags_count (Hash $count)                        { ... }
+
+    method read_tags_index                                       { ... }
+    method write_tags_index (Hash $index)                        { ... }
+
+    method get_tag_count ($page)                                { ... }
+
+
+    method save_page($page, $new_text, $author, $tags) {
         my $modification_id = get_unique_id();
 
         my $page_history = self.read_page_history($page);
@@ -88,8 +126,12 @@ class Storage {
 
         self.write_modification( $modification_id, 
                                  [ $page, $new_text, $author] );
+        
+        my $old_tags = self.read_page_tags($page); 
+        self.remove_tags($page, $old_tags);
+        self.add_tags($page, $tags);
 
-        self.add_recent_change( $modification_id );
+        self.write_page_tags($page, $tags);
     }
 
     method add_recent_change( $modification_id ) {
@@ -110,11 +152,17 @@ class Storage::File is Storage {
     my $.content_path        is rw;
     my $.modifications_path  is rw;
     my $.recent_changes_path is rw;
+    my $.page_tags_path      is rw;
+    my $.tags_count_path     is rw;
+    my $.tags_index_path     is rw;
 
     method init {
         $.content_path = 'data/articles/';
         $.modifications_path = 'data/modifications/';
         $.recent_changes_path = 'data/recent-changes';
+        $.page_tags_path = 'data/page_tags/';
+        $.tags_count_path = 'data/tags_count';
+        $.tags_index_path = 'data/tags_index';
     }
 
     method wiki_page_exists($page) {
@@ -161,6 +209,121 @@ class Storage::File is Storage {
         my $fh = open( $file, :w );
         $fh.say( $data );
         $fh.close();
+    }
+
+    method add_tags ($page, $tags) {
+
+        # RAKUDO: return with modificaton parsed wrong
+        return 1 if $tags ~~ m/^ \s* $/;
+        my $count = self.read_tags_count;
+
+        my @tags = tags_parse($tags);
+        for @tags -> $t {
+            # RAKUDO: Increment not implemented in class 'Undef'
+            if $count{$t} {
+                $count{$t}++;
+            } 
+            else {
+                $count{$t} = 1;
+            }
+        }
+
+        self.write_tags_count($count);
+
+        my $index = self.read_tags_index;
+
+        for @tags -> $t {
+            # I think it`s must be Hash of Arrays, but right now look like 
+            # with Hash of Hashes it`s simply to realize.
+            unless $index{$t} {
+                $index{$t} = {};
+            }
+            unless $index{$t}{$page} {
+                $index{$t}{$page} = 1;
+            }
+
+        }
+
+        self.write_tags_index($index);
+    }
+
+    method remove_tags($page, $tags) {
+        
+        # RAKUDO: return with modificaton parsed wrong
+        return 1 if $tags ~~ m/^ \s* $/;
+
+        my $count = self.read_tags_count;
+
+        my @tags = tags_parse($tags);
+        for @tags -> $t {
+            # RAKUDO: Decrement not implemented in class 'Undef'
+            if $count{$t} && $count{$t} > 0 {
+                $count{$t}--;
+            } 
+            else {
+                $count{$t} = 0;
+            }
+        }
+
+        self.write_tags_count($count);
+
+        my $index = self.read_tags_index;
+
+        for @tags -> $t {
+            # I think it`s must be Hash of Arrays, but right now look like with
+            # Hash of Hashes it`s simply to realize.
+            if $index{$t} && $index{$t}{$page} {
+                    $index{$t}{$page} = 0;
+            }
+        }
+        self.write_tags_index($index);
+    }
+
+    method read_page_tags($page) {
+        my $file = $.page_tags_path ~ $page;
+        # RAKUDO: use :e
+        return '' unless file_exists( $file );
+        return slurp($file);
+    }
+
+    method write_page_tags ($page, $tags) {
+        my $file =  $.page_tags_path ~ $page;
+        my $fh = open( $file, :w );
+        $fh.say( $tags );
+        $fh.close();
+    }
+   
+    method read_tags_count {
+        my $file = $.tags_count_path;
+        # RAKUDO: use :e
+        return {} unless file_exists($file);
+        return eval( slurp($file) );
+    }
+
+    method write_tags_count (Hash $counts) {
+        my $file =  $.tags_count_path;
+        my $fh = open( $file, :w );
+        $fh.say( $counts.perl );
+        $fh.close();
+    }
+
+    method read_tags_index {  
+        my $file = $.tags_index_path;
+        # RAKUDO: use :e
+        return {} unless file_exists($file);
+        return eval( slurp($file) );
+    }
+
+    method write_tags_index (Hash $index) {
+        my $file =  $.tags_index_path;
+        my $fh = open( $file, :w );
+        $fh.say( $index.perl );
+        $fh.close();
+    }
+
+    method get_tag_count ($tag)  {
+        my $counts = self.read_tags_count;
+        return $counts{$tag};
     }
 }
 
@@ -220,6 +383,35 @@ class Wiki does Session {
         $template.param('CONTENT'   => $converter.format(
                                            $.storage.read_page($page)
                                        ));
+
+        my $page_tags = $.storage.read_page_tags($page);
+        my @page_tags = tags_parse($page_tags); 
+
+        # does exist clearest way to check @tags... mb @t ~~ [] ?
+        if @page_tags[0] {
+            @page_tags = map { '<a class="t' ~ $.storage.get_tag_count($_) 
+                ~ '" href="?action=toc?tag=' ~ $_ ~'">' ~ $_ ~ '</a>'}, @page_tags;
+
+            $page_tags = @page_tags.join(', ');
+        }
+    
+        $template.param('PAGETAGS' => $page_tags);
+
+        my $tags = $.storage.read_tags_count;
+
+        my $tags_str;
+        if $tags {
+            for $tags.keys -> $tag {
+                if $tags{$tag} > 0 {
+                    $tags_str = $tags_str ~ '<a class="t' 
+                        ~ tag_count_normalize( $tags{$tag} ) 
+                        ~ '" href="?action=toc?tag=' ~ $tag ~ '">' 
+                        ~ $tag ~ '</a> ';
+                }
+            }
+        }
+        $template.param('TAGS' => $tags_str);
+
         $template.param('LOGGED_IN' => self.logged_in());
 
         $.cgi.send_response(
@@ -251,11 +443,12 @@ class Wiki does Session {
         # The 'edit' action handles both showing the form and accepting the
         # POST data. The difference is the presence of the 'articletext'
         # parameter -- if there is one, the action is considered a save.
-        if $.cgi.param<articletext> {
+        if $.cgi.param<articletext> || $.cgi.param<tags> {
             my $new_text = $.cgi.param<articletext>;
+            my $tags = $.cgi.param<tags>;
             my $session_id = $.cgi.cookie<session_id>;
             my $author = $sessions{$session_id}<user_name>;
-            $.storage.save_page($page, $new_text, $author);
+            $.storage.save_page($page, $new_text, $author, $tags);
             return self.view_page();
         }
 
@@ -265,6 +458,8 @@ class Wiki does Session {
         $template.param('PAGE'      => $page);
         $template.param('TITLE'     => $title);
         $template.param('CONTENT'   => $old_content);
+
+        $template.param('TAGS'      => $.storage.read_page_tags($page));
         $template.param('LOGGED_IN' => True);
 
         $.cgi.send_response(
