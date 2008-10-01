21613454e642a144b87a7f767d520a4125ba7847
Johan Viklund <johan.viklund@gmail.com>
2008-10-01 20:39:33 +0200

    [p6w] Refactored out tag-handling into Tags.pm

--- p6w/November__Storage.pm
+++ p6w/November__Storage.pm
@@ -13,20 +13,6 @@ class November__Storage {
     method read_modification($modification_id)                   { ... }
     method write_modification( $modification_id, $modification ) { ... }
 
-    method read_page_tags ($page)                                { ... }
-    method write_page_tags ($page, $tags)                        { ... }
-
-    method add_tags ($page, $tags)                               { ... }
-    method remove_tags ($page, $tags)                            { ... }
-
-    method read_tags_count                                       { ... }
-    method write_tags_count (Hash $count)                        { ... }
-
-    method read_tags_index                                       { ... }
-    method write_tags_index (Hash $index)                        { ... }
-
-    method get_tag_count ($page)                                { ... }
-
 
     method save_page($page, $new_text, $author, $tags) {
         my $modification_id = get_unique_id();
@@ -37,12 +23,6 @@ class November__Storage {
 
         self.write_modification( $modification_id, 
                                  [ $page, $new_text, $author] );
-        
-        my $old_tags = self.read_page_tags($page); 
-        self.remove_tags($page, $old_tags);
-        self.add_tags($page, $tags);
-
-        self.write_page_tags($page, $tags);
     }
 
     method add_recent_change( $modification_id ) {
--- p6w/November__Storage__File.pm
+++ p6w/November__Storage__File.pm
@@ -6,9 +6,6 @@ class November__Storage__File is November__Storage {
     my $.content_path        = 'data/articles/';
     my $.modifications_path  = 'data/modifications/';
     my $.recent_changes_path = 'data/recent-changes';
-    my $.page_tags_path      = 'data/page_tags/';
-    my $.tags_count_path     = 'data/tags_count';
-    my $.tags_index_path     = 'data/tags_index';
 
     method wiki_page_exists($page) {
         return file_exists( $.content_path ~ $page );
@@ -54,127 +51,6 @@ class November__Storage__File is November__Storage {
         my $fh = open( $file, :w );
         $fh.say( $data );
         $fh.close();
-    }
-
-    method add_tags ($page, $tags) {
-
-        # RAKUDO: return with modificaton parsed wrong
-        return 1 if $tags ~~ m/^ \s* $/;
-        my $count = self.read_tags_count;
-
-        my @tags = tags_parse($tags);
-        for @tags -> $t {
-            # RAKUDO: Increment not implemented in class 'Undef'
-            if $count{$t} {
-                $count{$t}++;
-            } 
-            else {
-                $count{$t} = 1;
-            }
-        }
-
-        self.write_tags_count($count);
-
-        my $index = self.read_tags_index;
-
-        for @tags -> $t {
-            # I think it`s must be Hash of Arrays, but right now look like 
-            # with Hash of Hashes it`s simply to realize.
-            unless $index{$t} {
-                $index{$t} = {};
-            }
-            unless $index{$t}{$page} {
-                $index{$t}{$page} = 1;
-            }
-
-        }
-
-        self.write_tags_index($index);
-    }
-
-    method remove_tags($page, $tags) {
-        
-        # RAKUDO: return with modificaton parsed wrong
-        return 1 if $tags ~~ m/^ \s* $/;
-
-        my $count = self.read_tags_count;
-
-        my @tags = tags_parse($tags);
-        for @tags -> $t {
-            # RAKUDO: Decrement not implemented in class 'Undef'
-            if $count{$t} && $count{$t} > 0 {
-                $count{$t}--;
-            } 
-            else {
-                $count{$t} = 0;
-            }
-        }
-
-        self.write_tags_count($count);
-
-        my $index = self.read_tags_index;
-
-        for @tags -> $t {
-            # I think it`s must be Hash of Arrays, but right now look like with
-            # Hash of Hashes it`s simply to realize.
-            if $index{$t} && $index{$t}{$page} {
-                    $index{$t}{$page} = 0;
-            }
-        }
-        self.write_tags_index($index);
-    }
-
-    method read_page_tags($page) {
-        my $file = $.page_tags_path ~ $page;
-        # RAKUDO: use :e
-        return '' unless file_exists( $file );
-        return slurp($file);
-    }
-
-    method write_page_tags ($page, $tags) {
-        my $file =  $.page_tags_path ~ $page;
-        my $fh = open( $file, :w );
-        $fh.say( $tags );
-        $fh.close();
-    }
-   
-    method read_tags_count {
-        my $file = $.tags_count_path;
-        # RAKUDO: use :e
-        return {} unless file_exists($file);
-        return eval( slurp($file) );
-    }
-
-    method write_tags_count (Hash $counts) {
-        my $file =  $.tags_count_path;
-        my $fh = open( $file, :w );
-        $fh.say( $counts.perl );
-        $fh.close();
-    }
-
-    method read_tags_index {  
-        my $file = $.tags_index_path;
-        # RAKUDO: use :e
-        return {} unless file_exists($file);
-        return eval( slurp($file) );
-    }
-
-    method write_tags_index (Hash $index) {
-        my $file =  $.tags_index_path;
-        my $fh = open( $file, :w );
-        $fh.say( $index.perl );
-        $fh.close();
-    }
-
-    method get_tag_count ($tag)  {
-        my $counts = self.read_tags_count;
-        unless $counts {
-            # TODO: I think we must say warn there, but look like it`s do not 
-            # implemented in Rakudo now. Or may be it`s not {warn "foo"} today. 
-            return 1;
-        }
-        return $counts{$tag};
-            
     }
 }
 
