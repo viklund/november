9a591cdc83e9ccbc792056e7cb99b48f51c5bc11
Ilya Belikin <forihrd@gmail.com>
2008-11-14 21:10:54 +1000

    [p6w] clear comment

--- p6w/URI/Grammar.pm
+++ p6w/URI/Grammar.pm
@@ -1,18 +1,22 @@
 grammar URI::Grammar {
-    #my($scheme, $authority, $path, $query, $fragment) =
-    #$uri =~ m|(?:([^:/?#]+):)?
-    #          (?://([^/?#]*))?
-    #          ([^?#]*)
-    #          (?:\?([^#]*))?
-    #          (?:#(.*))?|;
     token TOP        { ^ [<scheme> ':']? [ '//' <authority>]? <path> ['?' <query>]? ['#' <fragment>]? $ };
     token scheme     { <-[:/&?#]>+ };
     token authority  { <host> [':' <port>]? };
     token host       { <-[/&?#:]>* };
     token port       { \d+ };
-    token path       { <slash>? [ <chunk> '/'?]* }; # * here mb wrong, because that allow '' URI
+    token path       { <slash>? [ <chunk> '/'?]* }; # * mb wrong, because that allow '' URI
     token slash      { '/' };
     token chunk      { <-[/?#]>+ };
     token query      { <-[#]>* };
     token fragment   { .* };
 }
+
+# Official regexp (p5):
+# my($scheme, $authority, $path, $query, $fragment) =
+# $uri =~ m/
+#           (?:([^:/?#]+):)?
+#           (?://([^/?#]*))?
+#           ([^?#]*)
+#           (?:\?([^#]*))?
+#           (?:#(.*))?
+#         /x;
