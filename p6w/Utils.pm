ebd4aeb9d16938b0f013ca2165cc5f49c4069256
Ilya Belikin <forihrd@gmail.com>
2008-11-21 21:53:13 +1000

    [p6w] move r_remove to Utils

--- p6w/Utils.pm
+++ p6w/Utils.pm
@@ -1,5 +1,12 @@
 use v6;
 
+sub r_remove( $str is rw ) is export {
+    # RAKUDO: :g not implemented yet :( 
+    while $str ~~ /\\r/ {
+        $str = $str.subst( /\\r/, '' );
+    }
+}
+
 sub get_unique_id is export {
     # hopefully pretty unique ID
     return int(time%1000000/100) ~ time%100
