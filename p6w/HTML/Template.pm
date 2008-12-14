926a0bb9550d6771f19f07a208d86dcb25616614
Ilya Belikin <forihrd@gmail.com>
2008-12-14 22:00:34 +1000

    [p6w] clear HTML::Template a little

--- p6w/HTML/Template.pm
+++ p6w/HTML/Template.pm
@@ -3,106 +3,105 @@ use v6;
 use Text::Escape;
 use HTML::Template::Grammar;
 
-class HTML::Template {
-    has $.input;
-    has %!params;
+class HTML::Template;
 
-    method from_string( Str $input ) {
-        return self.new(input => $input);
-    }
+has $!in;
+has %!params;
 
-    method from_file($file_path) {
-        return self.from_string( slurp($file_path) );
-    }
+method from_string( Str $in ) {
+    return self.new(in => $in);
+}
 
-    method param( Pair $param ) {
-        %!params{$param.key} = $param.value;
-    }
+method from_file($file_path) {
+    return self.from_string( slurp($file_path) );
+}
 
-    method with_params( Hash %params ) {
-        %!params = %params;
-        return self;
-    }
+method param( Pair $param ) {
+    %!params{$param.key} = $param.value;
+}
 
-    method output() {
-        return substitute( parse($.input), %!params );
-    }
+method with_params( Hash %params ) {
+    %!params = %params;
+    return self;
+}
 
-    sub parse( Str $in ) {
-        # RAKUDO: when #58676 will be resolved use: 
-        # $in ~~ HTML::Template::Grammar.new;
-        $in ~~ HTML::Template::Grammar::TOP;
-        die("No match") unless ~$/;
-        return $/<contents>;
-    }
+method output() {
+    return self.substitute( self.parse, %!params );
+}
 
-    # We doing this using {*} in asterisk brunch.
-    sub substitute( $contents, %params ) {
-        my $output = ~$contents<plaintext>;
+method parse( $in? ) {
+    # RAKUDO: when #58676 will be resolved use: 
+    # $in ~~ HTML::Template::Grammar.new;
+    ($in || $!in) ~~ HTML::Template::Grammar::TOP;
+    die("No match") unless $/;
+    return $/<contents>;
+}
 
-        for ($contents<chunk> // ()) -> $chunk {
+method substitute( $contents, %params ) {
+    my $output = ~$contents<plaintext>;
 
-            if $chunk<directive><insertion> -> $i {
-                my $key = $i<attributes><name>;
-                my $value = %params{$key};
+    for ($contents<chunk> // ()) -> $chunk {
 
-                if $i<attributes><escape> {
-                    my $et = ~$i<attributes><escape>[0];
-                    # RAKUDO: Segaful here :(
-                    #$value = escape($value, $et);
-                    if $et eq 'HTML' {
-                        $value = escape($value, 'HTML');
-                    } 
-                    elsif $et eq 'URL' | 'URI' {
-                        $value = escape($value, 'URL');
-                    }
+        if $chunk<directive><insertion> -> $i {
+            my $key = ~$i<attributes><name>;
+            my $value = %params{$key};
 
+            if $i<attributes><escape> {
+                my $et = ~$i<attributes><escape>[0];
+                # RAKUDO: Segaful here :(
+                #$value = escape($value, $et);
+                if $et eq 'HTML' {
+                    $value = escape($value, 'HTML');
+                } 
+                elsif $et eq 'URL' | 'URI' {
+                    $value = escape($value, 'URL');
                 }
-                $output ~= ~$value;
+
             }
-            elsif $chunk<directive><if_statement> {
-                my $key = ~$chunk<directive><if_statement><attributes><name>;
-                my $condition = %params{$key};
-                if $condition {
-                    $output ~= substitute(
-                                 $chunk<directive><if_statement><contents>,
-                                 %params
-                               );
-                }
-                elsif $chunk<directive><if_statement><else> {
-                    $output ~= substitute(
-                                 $chunk<directive><if_statement><else>[0],
-                                 %params
-                               );
-                }
-            }
-            elsif $chunk<directive><for_statement> {
-                my $key = ~$chunk<directive><for_statement><attributes><name><val>;
-                my $iterations = %params{$key};
-                # RAKUDO: This should exhibit the correct behaviour, but due
-                # to a bug having to do with for loops and recursion, it
-                # doesn't. [perl #58392]
-                for $iterations.values -> $iteration {
-                    $output ~= substitute(
-                                 $chunk<directive><for_statement><contents>,
-                                 $iteration
-                               );
-                }
-            }
-            elsif $chunk<directive><include> {
-                my $file = ~$chunk<directive><include><attributes><name><val>;
-                if $file ~~ :e  {
-                    $output ~= substitute(
-                                 parse( slurp($file) ),
-                                 %params
-                             );
-                }
-            }
-
-            $output ~= ~$chunk<plaintext>;
+            $output ~= ~$value;
         }
-        return $output;
+        elsif $chunk<directive><if_statement> {
+            my $key = ~$chunk<directive><if_statement><attributes><name>;
+            my $condition = %params{$key};
+            if $condition {
+                $output ~= self.substitute(
+                                $chunk<directive><if_statement><contents>,
+                                %params
+                            );
+            }
+            elsif $chunk<directive><if_statement><else> {
+                $output ~= self.substitute(
+                                $chunk<directive><if_statement><else>[0],
+                                %params
+                            );
+            }
+        }
+        elsif $chunk<directive><for_statement> {
+            my $key = ~$chunk<directive><for_statement><attributes><name><val>;
+            my $iterations = %params{$key};
+            # RAKUDO: This should exhibit the correct behaviour, but due
+            # to a bug having to do with for loops and recursion, it
+            # doesn't. [perl #58392]
+            for $iterations.values -> $iteration {
+                $output ~= self.substitute(
+                                $chunk<directive><for_statement><contents>,
+                                $iteration
+                            );
+            }
+        }
+        elsif $chunk<directive><include> {
+            my $file = ~$chunk<directive><include><attributes><name><val>;
+            if $file ~~ :e  {
+                $output ~= self.substitute(
+                                self.parse( slurp($file) ),
+                                %params
+                            );
+            }
+        }
+
+        $output ~= ~$chunk<plaintext>;
     }
+    return $output;
 }
 
 # vim:ft=perl6
