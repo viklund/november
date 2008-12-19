f0e384bbfedc995de1a26df18b9189351f74e1b9
Ilya Belikin <forihrd@gmail.com>
2008-12-20 03:42:38 +1000

    [p6w] merge Jeff++ Config.pm with small modification. Add config.example, use just config for work. We should review all this stuff.

--- p6w/Config.pm
+++ p6w/Config.pm
@@ -1,18 +1,19 @@
 use v6;
+class Config {
+    our $config_file = 'config';
+    our $.server_root;
+    our $.web_root;
+    our $.skin;
+}
 
-class Config;
+# RAKUDO: merge with class when we can set attribute values in the declaration
+$Config::config_file ~~ :e or die "couldn't open config file";
+my %config = eval slurp $Config::config_file;
 
-method server_root {
-    return '';
-} 
-
-method web_root {
-    return '';
-} 
-
-method skin {
-    return 'Autumn';
-} 
-
+Config.server_root = %config<server_root>;
+Config.web_root    = %config<web_root>;
+Config.skin        = %config<skin>;
+ 
 # vim:ft=perl6
+  
 
--- p6w/November/Storage/File.pm
+++ p6w/November/Storage/File.pm
@@ -6,6 +6,9 @@ use Config;
 
 class November::Storage::File is November::Storage;
 
+# RAKUDO: initial attr value do not fully works 
+# given Config.server_root { # and use $_ inside. 
+# But we can`t do that now because "my".
 my $.content_path        = Config.server_root ~ 'data/articles/';
 my $.modifications_path  = Config.server_root ~ 'data/modifications/';
 my $.recent_changes_path = Config.server_root ~ 'data/recent-changes';
