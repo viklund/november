4f806e2cd171e75614ab77a4c66a24c400256d79
Carl Masak <cmasak@gmail.com>
2009-01-09 21:30:43 +0100

    [p6w] removed config.example and put its contents back in Config.pm

--- p6w/Config.pm
+++ p6w/Config.pm
@@ -1,19 +1,10 @@
 use v6;
 class Config {
-    our $config_file = 'config';
     our $.server_root;
     our $.web_root;
-    our $.skin;
+    our $.skin = 'Autumn';
 }
 
-# RAKUDO: merge with class when we can set attribute values in the declaration
-$Config::config_file ~~ :e or die "couldn't open config file";
-my %config = eval slurp $Config::config_file;
-
-Config.server_root = %config<server_root>;
-Config.web_root    = %config<web_root>;
-Config.skin        = %config<skin>;
- 
 # vim:ft=perl6
   
 
