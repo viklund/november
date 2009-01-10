c2c423bade75fc99f6d5d8ef55831232d33ae813
Carl Masak <cmasak@gmail.com>
2009-01-10 16:17:03 +0100

    [p6w] temporarily reinstated the braces in URI.pm since going without them causes test failures.

--- p6w/URI.pm
+++ p6w/URI.pm
@@ -1,86 +1,87 @@
 use v6;
 
-class URI;
+class URI {
 
-# RAKUDO: Match object does not do assignment properly :(
-#my Match $.parts; dies in init with 'Type mismatch in assignment';
-# workaround:
-has $.uri;
-has @.chunks;
-
-method init ($str) {
-    use URI::Grammar;
-
-    # clear string before parsing
-    my $c_str = $str;
-    $c_str .= subst(/^ \s* ['<' | '"'] /, '');
-    $c_str .= subst(/ ['>' | '"'] \s* $/, '');
-
-    # RAKUDO: that is wrong, but works now
-    $c_str ~~ URI::Grammar::TOP;
-    unless $/ { die "Could not parse URI: $str" }
-
-    $!uri = $/;
-
-    @!chunks = $/<path><chunk> // ('');
-}
-
-method scheme {
-    my $s = $.uri<scheme> // '';
-    # RAKUDO: return 1 if use ~ below die because can`t do lc on Math after
-    return ~$s.lc;
-}
-
-method authority {
-    my $a = $.uri<authority> // '';
-    # RAKUDO: return 1 if use ~ below die because can`t do lc on Math after
-    return ~$a.lc;
-}
-
-method host {
-    #RAKUDO: $.uri<authority>[0]<host> return full <authority> now
-    my $h = ~$.uri<authority>[0]<host>;
-    return $h.lc // '';
-}
-
-method port {
-    # TODO: send rakudobug
-    # RAKUDO: $.uri<authority><port> return full <authority> now
+    # RAKUDO: Match object does not do assignment properly :(
+    #my Match $.parts; dies in init with 'Type mismatch in assignment';
     # workaround:
-    item $.uri<authority>[0]<port> // '';
-}
+    has $.uri;
+    has @.chunks;
 
-method path {
-    my $p = ~$.uri<path> // '';
-    return $p.lc;
-}
+    method init ($str) {
+        use URI::Grammar;
 
-method absolute {
-    return ?($.uri<path><slash> // $.scheme);
-}
+        # clear string before parsing
+        my $c_str = $str;
+        $c_str .= subst(/^ \s* ['<' | '"'] /, '');
+        $c_str .= subst(/ ['>' | '"'] \s* $/, '');
 
-method relative {
-    return !($.uri<path><slash> // $.scheme);
-}
+        # RAKUDO: that is wrong, but works now
+        $c_str ~~ URI::Grammar::TOP;
+        unless $/ { die "Could not parse URI: $str" }
 
-method query {
-    item $.uri<query> // '';
-}
-method frag {
-    my $f = $.uri<fragment> // '';
-    return ~$f.lc;
-}
+        $!uri = $/;
 
-method fragment { $.frag }
+        @!chunks = $/<path><chunk> // ('');
+    }
 
-method Str() {
-    my $str;
-    $str ~= $.scheme if $.scheme;
-    $str ~= '://' ~ $.authority if $.authority;
-    $str ~= $.path;
-    $str ~= '?' ~ $.query if $.query;
-    $str ~= '#' ~ $.frag if $.frag;
-    return $str; 
+    method scheme {
+        my $s = $.uri<scheme> // '';
+        # RAKUDO: return 1 if use ~ below die because can`t do lc on Math after
+        return ~$s.lc;
+    }
+
+    method authority {
+        my $a = $.uri<authority> // '';
+        # RAKUDO: return 1 if use ~ below die because can`t do lc on Math after
+        return ~$a.lc;
+    }
+
+    method host {
+        #RAKUDO: $.uri<authority>[0]<host> return full <authority> now
+        my $h = ~$.uri<authority>[0]<host>;
+        return $h.lc // '';
+    }
+
+    method port {
+        # TODO: send rakudobug
+        # RAKUDO: $.uri<authority><port> return full <authority> now
+        # workaround:
+        item $.uri<authority>[0]<port> // '';
+    }
+
+    method path {
+        my $p = ~$.uri<path> // '';
+        return $p.lc;
+    }
+
+    method absolute {
+        return ?($.uri<path><slash> // $.scheme);
+    }
+
+    method relative {
+        return !($.uri<path><slash> // $.scheme);
+    }
+
+    method query {
+        item $.uri<query> // '';
+    }
+    method frag {
+        my $f = $.uri<fragment> // '';
+        return ~$f.lc;
+    }
+
+    method fragment { $.frag }
+
+    method Str() {
+        my $str;
+        $str ~= $.scheme if $.scheme;
+        $str ~= '://' ~ $.authority if $.authority;
+        $str ~= $.path;
+        $str ~= '?' ~ $.query if $.query;
+        $str ~= '#' ~ $.frag if $.frag;
+        return $str; 
+    }
 }
 
 =begin pod
