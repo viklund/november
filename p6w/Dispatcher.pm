680702920fde88618eb3d0dedb3ef5109d8b200c
Carl Masak <cmasak@gmail.com>
2009-01-10 16:25:14 +0100

    [p6w] temporarily switched to braces in Dispatcher.pm the semicolon notation currently causes test failures

--- p6w/Dispatcher.pm
+++ p6w/Dispatcher.pm
@@ -1,46 +1,47 @@
 use v6;
-class Dispatcher;
-
 use Dispatcher::Rule;
 
-has @.rules;
-has $.default is rw;
+class Dispatcher {
 
-multi method add (Dispatcher::Rule $rule) {
-    die "Only complete rules allowed" unless $rule.?is_complete;
-    @!rules.push($rule);
-}
+    has @.rules;
+    has $.default is rw;
 
-multi method add (@tokens, $action){
-    my $rule = Dispatcher::Rule.new( tokens => @tokens.list, action => $action );
-    @!rules.push($rule);
-}
-
-# I think a Hash might be better here, but Rakudo converts all hash keys
-# into string now
-method add_rules(@rules) {
-    # RAKUDO: rakudo doesn't know return values in for loops yet
-    my $r;
-    for @rules.list -> $tokens, $action {
-        $r = self.add([$tokens.list], $action);
+    multi method add (Dispatcher::Rule $rule) {
+        die "Only complete rules allowed" unless $rule.?is_complete;
+        @!rules.push($rule);
     }
-    return $r;
-}
 
-method dispatch (@chunks) {
-    my @matched =  @!rules.grep: { .match(@chunks) };    
+    multi method add (@tokens, $action){
+        my $rule = Dispatcher::Rule.new( tokens => @tokens.list, action => $action );
+        @!rules.push($rule);
+    }
 
-    if @matched {
-        # RAKUDO: [*-1] do not work in array-attribute [perl #61766] 
-        my $result = @matched[@matched.end].apply;
-        .clear for @!rules; 
-        return $result;
+    # I think a Hash might be better here, but Rakudo converts all hash keys
+    # into string now
+    method add_rules(@rules) {
+        # RAKUDO: rakudo doesn't know return values in for loops yet
+        my $r;
+        for @rules.list -> $tokens, $action {
+            $r = self.add([$tokens.list], $action);
+        }
+        return $r;
     }
-    elsif defined $.default {
-        $.default();
-    }
-    else {
-        return Failure;
+
+    method dispatch (@chunks) {
+        my @matched =  @!rules.grep: { .match(@chunks) };    
+
+        if @matched {
+            # RAKUDO: [*-1] do not work in array-attribute [perl #61766] 
+            my $result = @matched[@matched.end].apply;
+            .clear for @!rules; 
+            return $result;
+        }
+        elsif defined $.default {
+            $.default();
+        }
+        else {
+            return Failure;
+        }
     }
 }
 
