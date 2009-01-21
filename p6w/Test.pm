58960dbbaa72405484d8fd27dd34de9a01037440
Johan Viklund <johan.viklund@gmail.com>
2009-01-20 20:47:01 -0500

    [p6w] Updated Test.pm with improvements from Rakudo

--- p6w/Test.pm
+++ p6w/Test.pm
@@ -14,6 +14,8 @@ our $num_of_tests_planned;
 our $todo_upto_test_num = 0;
 our $todo_reason = '';
 
+our $*WARNINGS = 0;
+
 # for running the test suite multiple times in the same process
 our $testing_started;
 
@@ -27,124 +29,123 @@ sub approx ($x, $y) {
     ($diff < $epsilon);
 }
 
-sub plan($number_of_tests) {
+sub plan($number_of_tests) is export() {
     $testing_started      = 1;
     $num_of_tests_planned = $number_of_tests;
 
     say '1..' ~ $number_of_tests;
 }
 
-multi sub pass($desc) {
+multi sub pass($desc) is export() {
     proclaim(1, $desc);
 }
 
-multi sub ok($cond, $desc) {
+multi sub ok(Object $cond, $desc) is export() {
     proclaim($cond, $desc);
 }
 
-multi sub ok($cond) { ok($cond, ''); }
+multi sub ok(Object $cond) is export() { ok($cond, ''); }
 
 
-multi sub nok($cond, $desc) {
+multi sub nok(Object $cond, $desc) is export() {
     proclaim(!$cond, $desc);
 }
 
-multi sub nok($cond) { nok(!$cond, ''); }
+multi sub nok(Object $cond) is export() { nok(!$cond, ''); }
 
 
-multi sub is($got, $expected, $desc) {
+multi sub is(Object $got, Object $expected, $desc) is export() {
     my $test = $got eq $expected;
     proclaim($test, $desc, $got, $expected);
 }
 
-multi sub is($got, $expected) { is($got, $expected, ''); }
+multi sub is(Object $got, Object $expected) is export() { is($got, $expected, ''); }
 
 
-multi sub isnt($got, $expected, $desc) {
+multi sub isnt(Object $got, Object $expected, $desc) is export() {
     my $test = !($got eq $expected);
     proclaim($test, $desc, $got, $expected);
 }
 
-multi sub isnt($got, $expected) { isnt($got, $expected, ''); }
+multi sub isnt(Object $got, Object $expected) is export() { isnt($got, $expected, ''); }
 
-multi sub is_approx($got, $expected, $desc) {
+multi sub is_approx(Object $got, Object $expected, $desc) is export() {
     my $test = abs($got - $expected) <= 0.00001;
     proclaim($test, $desc, $got, $expected);
 }
 
-multi sub is_approx($got, $expected) { is_approx($got, $expected, ''); }
+multi sub is_approx($got, $expected) is export() { is_approx($got, $expected, ''); }
 
-multi sub todo($reason, $count) {
+multi sub todo($reason, $count) is export() {
     $todo_upto_test_num = $num_of_tests_run + $count;
     $todo_reason = '# TODO ' ~ $reason;
 }
 
-multi sub todo($reason) {
+multi sub todo($reason) is export() {
     $todo_upto_test_num = $num_of_tests_run + 1;
     $todo_reason = '# TODO ' ~ $reason;
 }
 
-multi sub skip()                { proclaim(1, "# SKIP"); }
-multi sub skip($reason)         { proclaim(1, "# SKIP " ~ $reason); }
-multi sub skip($count, $reason) {
+multi sub skip()                is export() { proclaim(1, "# SKIP"); }
+multi sub skip($reason)         is export() { proclaim(1, "# SKIP " ~ $reason); }
+multi sub skip($count, $reason) is export() {
     for 1..$count {
         proclaim(1, "# SKIP " ~ $reason);
     }
 }
 
-multi sub skip_rest() {
+multi sub skip_rest() is export() {
     skip($num_of_tests_planned - $num_of_tests_run, "");
 }
 
-multi sub skip_rest($reason) {
+multi sub skip_rest($reason) is export() {
     skip($num_of_tests_planned - $num_of_tests_run, $reason);
 }
 
-sub diag($message) { say '# '~$message; }
+sub diag($message) is export() { say '# '~$message; }
 
 
-multi sub flunk($reason) { proclaim(0, "flunk $reason")}
+multi sub flunk($reason) is export() { proclaim(0, "flunk $reason")}
 
 
-multi sub isa_ok($var,$type) {
+multi sub isa_ok($var,$type) is export() {
     ok($var.isa($type), "The object is-a '$type'");
 }
-multi sub isa_ok($var,$type, $msg) { ok($var.isa($type), $msg); }
+multi sub isa_ok($var,$type, $msg) is export() { ok($var.isa($type), $msg); }
 
-multi sub dies_ok($closure, $reason) {
+multi sub dies_ok($closure, $reason) is export() {
     try {
         $closure();
     }
     proclaim((defined $!), $reason);
 }
-multi sub dies_ok($closure) {
+multi sub dies_ok($closure) is export() {
     dies_ok($closure, '');
 }
 
-multi sub lives_ok($closure, $reason) {
+multi sub lives_ok($closure, $reason) is export() {
     try {
         $closure();
     }
     proclaim((not defined $!), $reason);
 }
-multi sub lives_ok($closure) {
+multi sub lives_ok($closure) is export() {
     lives_ok($closure, '');
 }
 
-multi sub eval_dies_ok($code, $reason) {
+multi sub eval_dies_ok($code, $reason) is export() {
     proclaim((defined eval_exception($code)), $reason);
 }
-multi sub eval_dies_ok($code) {
+multi sub eval_dies_ok($code) is export() {
     eval_dies_ok($code, '');
 }
 
-multi sub eval_lives_ok($code, $reason) {
+multi sub eval_lives_ok($code, $reason) is export() {
     proclaim((not defined eval_exception($code)), $reason);
 }
-multi sub eval_lives_ok($code) {
+multi sub eval_lives_ok($code) is export() {
     eval_lives_ok($code, '');
 }
-
 
 
 multi sub is_deeply($this, $that, $reason) {
@@ -159,7 +160,7 @@ multi sub is_deeply($this, $that) {
 
 sub _is_deeply( $this, $that) {
 
-    if $this ~~ Array && $that ~~ Array {
+    if $this ~~ List && $that ~~ List {
         return if +$this.values != +$that.values;
         for $this Z $that -> $a, $b {
             return if ! _is_deeply( $a, $b );
@@ -178,7 +179,7 @@ sub _is_deeply( $this, $that) {
         return $this eq $that;
     }
     elsif $this ~~ Pair && $that ~~ Pair {
-        return $this.key eq $that.key 
+        return $this.key eq $that.key
                && _is_deeply( $this.value, $this.value );
     }
     elsif $this ~~ undef && $that ~~ undef && $this.WHAT eq $that.WHAT {
@@ -188,6 +189,7 @@ sub _is_deeply( $this, $that) {
     return;
 }
 
+
 ## 'private' subs
 
 sub eval_exception($code) {
@@ -196,9 +198,13 @@ sub eval_exception($code) {
     $eval_exception // $!;
 }
 
-sub proclaim($cond, $desc, $got?, $expected?) {
+sub proclaim(Object $cond, $desc, $got?, $expected?) {
     $testing_started  = 1;
     $num_of_tests_run = $num_of_tests_run + 1;
+
+    if $cond.HOW().isa($cond, Junction) {
+        warn("Junction passed to proclaim");
+    }
 
     unless $cond {
         print "not ";
@@ -206,7 +212,6 @@ sub proclaim($cond, $desc, $got?, $expected?) {
             unless  $num_of_tests_run <= $todo_upto_test_num;
     }
     print "ok ", $num_of_tests_run, " - ", $desc;
-
     if $todo_reason and $num_of_tests_run <= $todo_upto_test_num {
         print $todo_reason;
     }
