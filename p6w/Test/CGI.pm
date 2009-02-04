323325d565011e2f22eb4c4184d73bb2f56ad4f9
Johan Viklund <johan.viklund@gmail.com>
2009-02-04 00:44:42 -0500

    [p6w] Added Test::CGI to be able to test entire wikiworkflow and updated tests

---
+++ p6w/Test/CGI.pm
@@ -0,0 +1,16 @@
+use CGI;
+class Test::CGI is CGI {
+
+    has $.response is rw;
+    has %.response_opts is rw;
+
+    method send_response($contents, %opts?) {
+        $.response = $contents;
+        if %opts {
+            %.response_opts = %opts;
+        }
+    }
+
+}
+
+# vim:ft=perl6
