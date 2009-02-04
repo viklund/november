223d56c6e2d09b6bde4515b13680dfae10ecc334
Johan Viklund <johan.viklund@gmail.com>
2009-02-04 00:44:02 -0500

    [p6w] Config.pm is now an object and hence more dynamic

--- p6w/Config.pm
+++ p6w/Config.pm
@@ -1,8 +1,19 @@
 use v6;
 class Config {
-    our $.server_root = '';
-    our $.web_root = '';
-    our $.skin = 'CleanAndSoft';
+    has $.server_root = '/home/johan/Devel/november/p6w/';
+    has $.web_root    = '';
+    has $.skin        = 'CleanAndSoft';
+    has $.markup      = 'Text::Markup::Wiki::MediaWiki';
+
+    method template_path {
+        my $str = $!server_root ~ 'skins/' ~ $!skin ~ '/';
+        return $str;
+    }
+
+    method userfile_path {
+        my $str = $!server_root ~ 'data/users';
+        return $str;
+    }
 }
 
 # vim:ft=perl6
