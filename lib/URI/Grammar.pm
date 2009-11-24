0da33d725f78434d76efcafe0bbb3673ee359be8
Jason Felds <jafelds@feather.(none)>
2009-11-24 16:08:09 +0100

    Force port to be capped at 65535 by IANA.

--- lib/URI/Grammar.pm
+++ lib/URI/Grammar.pm
@@ -4,7 +4,13 @@ grammar URI::Grammar {
     token scheme     { <-[:/&?#]>+ };
     token authority  { <host> [':' <port>]? };
     token host       { <-[/&?#:]>* };
-    token port       { ^\d**1..5$ }; # Need to limit this to 65535
+    token port       { <pt6553X>|<pt655XX>|<pt65XXX>|<pt6XXXX>|<pt10K>|<ptLow> };
+    token ptLow      { \d**1..4 };
+    token pt10K      { <[1..5]>\d**4 };
+    token pt6XXXX    { 6<[0..4]>\d**3 };
+    token pt65XXX    { 65<[0..4]>\d**2 };
+    token pt655XX    { 655<[0..2]>\d };
+    token pt6553X    { 6553<[0..5]> };
     token path       { <slash>? [ <chunk> '/'?]* }; # * mb wrong, because that allow '' URI
     token slash      { '/' };
     token chunk      { <-[/?#]>+ };
